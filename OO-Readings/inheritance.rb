0f1eeaa12e6e51d0cceaf9f0deb2e0909ca40aec
Phillip Johnson <pdjohnson84@gmail.com>
2016-10-08 17:42:37 -0700

    finished inheritance exercises

--- OO-Readings/inheritance.rb
+++ OO-Readings/inheritance.rb
@@ -1,6 +1,15 @@
-class MyCar
+module Towable
+  def can_tow?(pounds)
+    pounds < 2000 ? true : false
+  end
+
+end
+
+class Vehicle
   attr_accessor :color
   attr_reader :year
+
+  @@num_of_vehicles = 0
 
   def initialize(year, model, color)
     @year = year
@@ -13,8 +22,8 @@ class MyCar
     puts "#{miles / gallons} miles per gallon of gas"
   end
 
-  def to_s
-    "My car is a #{color}, #{year}, #{@model}!"
+  def self.num_of_vehicles
+    p "Number of vehicles: #{@@num_of_vehicles}"
   end
 
   def speed_up(number)
@@ -40,4 +49,64 @@ class MyCar
     self.color = new_color
     puts "Your new #{color} paint job looks great!"
   end
+
+  def age
+    puts "The vehicle is #{get_vehicle_age} years old"
+  end
+
+  private
+
+  def get_vehicle_age
+    Time.now.year - self.year
+  end
 end
+
+class MyCar < Vehicle
+  NUMBER_OF_DOORS = 4
+
+  def to_s
+    "My car is a #{color}, #{year}, #{@model}!"
+  end
+end
+
+class MyTruck < Vehicle
+  include Towable
+
+  NUMBER_OF_DOORS = 4
+end
+
+class Student
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
+
+  def better_grade_than?(other_student)
+    grade > other_student.grade
+  end
+
+  protected
+
+  def grade
+    @grade
+  end
+end
+
+lumina = MyCar.new(1997, 'chevy lumina', 'white')
+lumina.speed_up(20)
+lumina.current_speed
+lumina.speed_up(20)
+lumina.current_speed
+lumina.brake(20)
+lumina.current_speed
+lumina.brake(20)
+lumina.current_speed
+lumina.shut_down
+MyCar.gas_mileage(13, 351)
+lumina.spray_paint("red")
+lumina.age
+puts lumina
+
+joe = Student.new("Joe", 90)
+bob = Student.new("Bob", 84)
+puts "Well done!" if joe.better_grade_than?(bob)
