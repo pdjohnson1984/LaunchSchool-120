976189d41cc03e17baa101360d327e7fa487b228
Phillip Johnson <pdjohnson84@gmail.com>
2016-10-17 22:32:00 -0700

    Adding hard exercises

---
+++ OOP/hard1.rb
@@ -0,0 +1,99 @@
+# class SecretFile
+#   def initialize(secret_data, logger)
+#     @data = secret_data
+#     @logger = logger
+#   end
+#
+#   def data
+#     @logger.create_log_entry
+#     @data
+#   end
+# end
+#
+# class SecurityLogger
+#   def create_log_entry
+#     # ... implementation omitted ...
+#   end
+# end
+
+module Moveable
+  attr_accessor :speed, :heading
+  attr_writer :fuel_capacity, :fuel_efficiency
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+class WheeledVehicle
+  include Moveable
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+end
+
+class Watercraft
+  include Moveable
+
+  attr_accessor :hull_count, :propeller_count
+
+  def initialize(num_propellers, num_hulls, fuel_efficiency, fuel_capacity)
+    self.propeller_count = num_propellers
+    self.hull_count = num_hulls
+    self.fuel_efficiency = fuel_efficiency
+    self.fuel_capacity = fuel_capacity
+  end
+
+  def range
+    range_by_using_fuel = super
+    return range_by_using_fuel + 10
+  end
+end
+
+class Auto < WheeledVehicle
+  def initialize
+    # 4 tires are various tire pressures
+    super([30,30,32,32], 50, 25.0)
+  end
+end
+
+class Motorcycle < WheeledVehicle
+  def initialize
+    # 2 tires are various tire pressures
+    super([20,20], 80, 8.0)
+  end
+end
+
+class Catamaran < Watercraft
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    super(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+  end
+end
+
+class Motorboat < Watercraft
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    super(1, 1, km_traveled_per_liter, liters_of_fuel_capacity)
+  end
+end
+
+bike = Motorcycle.new
+puts bike.tire_pressure(0)
+bike.range
+
+car = Auto.new
+puts car.tire_pressure(0)
+puts car.tire_pressure(1)
+puts car.tire_pressure(2)
+puts car.tire_pressure(3)
+car.range
