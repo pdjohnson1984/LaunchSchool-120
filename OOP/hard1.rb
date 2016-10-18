3890764b3c90ada876acfee359d71674cc72ddcc
Phillip Johnson <pdjohnson84@gmail.com>
2016-10-17 22:33:03 -0700

    Forgot to save file

--- OOP/hard1.rb
+++ OOP/hard1.rb
@@ -1,20 +1,20 @@
-# class SecretFile
-#   def initialize(secret_data, logger)
-#     @data = secret_data
-#     @logger = logger
-#   end
-#
-#   def data
-#     @logger.create_log_entry
-#     @data
-#   end
-# end
-#
-# class SecurityLogger
-#   def create_log_entry
-#     # ... implementation omitted ...
-#   end
-# end
+class SecretFile
+  def initialize(secret_data, logger)
+    @data = secret_data
+    @logger = logger
+  end
+
+  def data
+    @logger.create_log_entry
+    @data
+  end
+end
+
+class SecurityLogger
+  def create_log_entry
+    # ... implementation omitted ...
+  end
+end
 
 module Moveable
   attr_accessor :speed, :heading
