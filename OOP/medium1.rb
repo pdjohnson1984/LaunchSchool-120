8f9da9f5162fea35ab82864c42610164a4ccd0f6
Phillip Johnson <pdjohnson84@gmail.com>
2016-10-17 20:49:00 -0700

    Adding exercises for medium 1

---
+++ OOP/medium1.rb
@@ -0,0 +1,55 @@
+class Greeting
+  def greet(greeting)
+    puts greeting
+  end
+end
+
+class Hello < Greeting
+  def hi
+    greet("Hello")
+  end
+end
+
+class Goodbye < Greeting
+  def bye
+    greet("Goodbye.")
+  end
+end
+
+
+hola = Hello.new
+adios = Goodbye.new
+
+hola.hi
+adios.bye
+
+class KrispyKreme
+  def initialize(filling_type, glazing)
+    @filling_type = filling_type
+    @glazing = glazing
+  end
+
+  def to_s
+    if !@filling_type && !@glazing
+      'Plain'
+    elsif @filling_type && !@glazing
+      @filling_type
+    elsif !@filling_type && @glazing
+      "Plain with #{@glazing}"
+    else
+      "#{@filling_type} with #{@glazing}"
+    end
+  end
+end
+
+donut1 = KrispyKreme.new(nil, nil)
+donut2 = KrispyKreme.new("Vanilla", nil)
+donut3 = KrispyKreme.new(nil, "sugar")
+donut4 = KrispyKreme.new(nil, "chocolate sprinkles")
+donut5 = KrispyKreme.new("Custard", "icing")
+
+puts donut1
+puts donut2
+puts donut3
+puts donut4
+puts donut5
